dc499811baddf727d6a1a197531f7d1c6805051c
Leslie G <lesliegonzalez@me.com>
2020-06-27 20:10:47 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-speed = num(40-60)
-speed 
+speed = (40...60)
+speed num < 40 || num > 60 ? true : false
 end
 
 
 
 def not_safe?(speed)
-	
+speed = num > 60 || num < 40 ? true : false
 end
 	
 
