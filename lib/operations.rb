8936b8f75efdae90dfb81110e39fb9f1d9536007
Leslie G <lesliegonzalez@me.com>
2020-06-27 20:36:11 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,9 @@
 def unsafe?(speed)
   speed = Range.new(40,60)
-  if speed == (40..60)
+  if (40..60) == (40...60)
   return false
 end
-  if speed > 60
+  if (61..160) > 60
   return true
   else speed < 40
   return true
